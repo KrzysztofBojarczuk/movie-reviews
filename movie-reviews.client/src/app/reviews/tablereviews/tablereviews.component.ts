6a5a0dbda49a1990066d6110bae73a26cbf200f8
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2024-03-31 09:06:17 +0200

    adding serviceand form

--- movie-reviews.client/src/app/reviews/tablereviews/tablereviews.component.ts
+++ movie-reviews.client/src/app/reviews/tablereviews/tablereviews.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { FormreviewsComponent } from '../formreviews/formreviews.component';
+import { Review } from '../../models/review';
+import { ReviewService } from '../../services/review.service';
 
 @Component({
   selector: 'app-tablereviews',
@@ -10,12 +12,27 @@ import { FormreviewsComponent } from '../formreviews/formreviews.component';
 export class TablereviewsComponent {
   ref: DynamicDialogRef | undefined;
 
-  constructor(private dialogService: DialogService) {}
+  review: Review[] = [];
+
+  constructor(
+    private dialogService: DialogService,
+    private reviewService: ReviewService
+  ) {}
+
+  ngOnInit(): void {
+    this.getReviews();
+  }
 
   openFormModal() {
     const ref = this.dialogService.open(FormreviewsComponent, {
       header: 'AddReview',
       width: '70%',
+    });
+  }
+
+  getReviews() {
+    this.reviewService.getAlReviews().subscribe((result) => {
+      this.review = result;
     });
   }
 }
