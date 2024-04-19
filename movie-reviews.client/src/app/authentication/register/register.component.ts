9ac19dff153826ff58a87313e3ec8f7293067cd5
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2024-04-19 07:07:57 +0200

    reactic forms

--- movie-reviews.client/src/app/authentication/register/register.component.ts
+++ movie-reviews.client/src/app/authentication/register/register.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/
+          ),
+          Validators.minLength(6),
+        ],
+      ],
+    });
+  }
 }
